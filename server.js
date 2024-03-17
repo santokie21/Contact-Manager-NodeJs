5b9d699adedeff1ffda35e826baf8cc2592e4efd
Santhosh S <officialuse3012@gmail.com>
2024-03-17 14:11:59 +0530

    get ports added and sending using json

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/contacts", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
