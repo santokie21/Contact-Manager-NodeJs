e00b9844fa6a9a9dc78768f761e1a8f54a431c45
Santhosh S <officialuse3012@gmail.com>
2024-03-17 14:43:07 +0530

    express routes & cantacts crud setuo

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -5,4 +5,20 @@ router.route("/").get((req, res) => {
   res.status(200).json({ message: "Get All contacts" });
 });
 
+router.route("/").post((req, res) => {
+  res.status(201).json({ message: "Create Contact" });
+});
+
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+});
+
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
+
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete Contact ${req.params.id}` });
+});
+
 module.exports = router;
