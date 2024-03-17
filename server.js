652073317de209b4a4ffa8103d96254de92f8904
Santhosh S <officialuse3012@gmail.com>
2024-03-17 14:24:21 +0530

    routes added

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/api/contacts", (req, res) => {
-  res.status(200).json({ message: "Get All contacts" });
-});
+app.use("/api/contacts", require("./routes/contactRoute"));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
