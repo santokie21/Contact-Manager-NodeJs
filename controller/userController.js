c801189a3d9508b80a08ef03c887225574358f44
santokie21 <officialuse3012@gmail.com>
2024-03-21 13:00:31 +0530

    register user added with hashed password

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,41 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../model/user");
 
 //@desc Register user
 //@route POST /api/users/register
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json("User registered");
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //create user
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //@desc Login user
