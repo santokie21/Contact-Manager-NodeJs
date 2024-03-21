efc1c875a2d86572c28dafb5bea0dc2776e7f4cb
santokie21 <officialuse3012@gmail.com>
2024-03-21 12:42:34 +0530

    user model added

---
+++ model/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please enter username"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+      unique: [true, "Email already exists"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
